50679fce6ee8c6305fdb85b54ba2cc25dd327906
Anujith123 <42373652+Anujith123@users.noreply.github.com>
2018-09-06 18:21:28 +0000

    Add files via upload

---
+++ lab5.5q8.cpp
@@ -0,0 +1,14 @@
+//load the library
+#include<iostream>
+using namespace std;
+int main(){
+           //giving rows
+           for(int i=0;i<5;++i){
+                                for(int j=0;j<=i;++j){
+                                                      cout<<"*";
+                                                                }
+                                cout<<endl;
+                                           }
+           //closing programs
+           return 0;
+                    }
